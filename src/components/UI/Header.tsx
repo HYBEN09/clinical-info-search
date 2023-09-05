819799f4c8aecddd26ec893b86b1b85df2581383
hyebeen byun <hyebeen2658@naver.com>
2023-09-05 18:02:12 +0900

    Design: Header 마크업 수정

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -11,6 +11,12 @@ export const Header = () => {
         <img src="/assets/logo.svg" alt="한국 임상정보 로고" height={36} />
         <A11yHidden>한국 임상정보</A11yHidden>
       </button>
+      <ul>
+        <li>소개</li>
+        <li>질문과 답변</li>
+        <li>소식받기</li>
+        <li>제휴/문의</li>
+      </ul>
     </HeaderWrapper>
   );
 };
@@ -20,6 +26,22 @@ const HeaderWrapper = styled.header`
   height: 80px;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  padding-left: 20px;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+
+  & > ul {
+    display: flex;
+    padding-right: 20px;
+
+    & > li {
+      margin-left: 30px;
+      font-weight: 600;
+      cursor: pointer;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
 `;
