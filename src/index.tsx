b558c0076b99f5b9a7e4f6dc040cdec4797c34d8
hyebeen byun <hyebeen2658@naver.com>
2023-09-05 15:06:12 +0900

    Style: 전역 스타일 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import './GlobalStyle.ts';
 import App from './App';
+import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
