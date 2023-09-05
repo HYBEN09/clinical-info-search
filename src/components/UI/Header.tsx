2513bb20dddae48b29f06bccb2f4d4bddc11b54f
hyebeen byun <hyebeen2658@naver.com>
2023-09-05 15:37:33 +0900

    Feat: Header 구현

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,3 +1,29 @@
+import { A11yHidden } from '@/constants/styled';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+
 export const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <HeaderWrapper>
+      <button onClick={navigateHome}>
+        <img src="/assets/logo.svg" alt="한국 임상정보 로고" height={36} />
+        <A11yHidden>한국 임상정보</A11yHidden>
+      </button>
+    </HeaderWrapper>
+  );
 };
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+`;
---
+++ src/constants/styled.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const A11yHidden = styled.h1`
+  overflow: hidden;
+  position: absolute !important;
+  clip: rect(0, 0, 0, 0);
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+`;
