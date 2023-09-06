301f7d968528c017c6db228a612cc6c5b2603785
hyebeen byun <hyebeen2658@naver.com>
2023-09-06 22:25:56 +0900

    Comment: 필요없는 주석 삭제

--- src/hooks/useRecentSearches.ts
+++ src/hooks/useRecentSearches.ts
@@ -6,19 +6,17 @@ export const useRecentSearches = () => {
   const [recentSearches, setRecentSearches] = useState<string[]>([]);
 
   useEffect(() => {
-    // 컴포넌트가 마운트될 때, 로컬 스토리지에서 저장된 검색어를 불러옴
     const storedSearches = localStorage.getItem('recentSearches');
     if (storedSearches) {
-      // 저장된 검색어를 최근 검색어 목록으로 설정
       setRecentSearches(JSON.parse(storedSearches));
     }
   }, []);
 
   // 새로운 검색어를 최근 검색어 목록에 추가하는 함수
   const addRecentSearch = (searchTerm: string) => {
-    const updatedSearches = [...recentSearches]; // 기존 검색어 목록을 복사
-    updatedSearches.unshift(searchTerm); // 새로운 검색어를 목록 맨 앞에 추가ㅍ
-    setRecentSearches(updatedSearches); // 최근 검색어 목록 갱신
+    const updatedSearches = [...recentSearches];
+    updatedSearches.unshift(searchTerm);
+    setRecentSearches(updatedSearches);
     localStorage.setItem('recentSearches', JSON.stringify(updatedSearches));
   };
 
